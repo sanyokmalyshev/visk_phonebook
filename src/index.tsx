5eb429429adc25e708e6c03dc3634458e48e4b0e
Oleksandr Malyshev <sanyokmalyshev@gmail.com>
2022-12-26 13:40:58 +0700

    Change routing

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from 'app/store';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 reportWebVitals();
